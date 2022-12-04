224224456ed3dabfc0c1447ea6d4aa9b834f61ae
Alexandr Bochev <abochev@gmail.com>
2022-12-04 14:25:43 +0200

    done

--- js/main.js
+++ js/main.js
@@ -41,5 +41,15 @@ const playDrum = (event) => {
   }
 }
 
+const setVH = () => {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`)
+}
+setVH()
+
+window.addEventListener('resize', () => {
+  setTimeout(setVH, 100);
+})
+
 drumkit.addEventListener('click', playDrum);
 drumkit.addEventListener('touchstart', playDrum);
